f03ff0839af3b654d19114fb44fb7971ff8d5335
Paul Knopf <pauldotknopf@gmail.com>
2018-10-21 17:36:46 -0400

    Checkpoint.

--- gen-image.sh
+++ gen-image.sh
@@ -2,6 +2,7 @@
 
 . ./.vars.sh
 
+cp $DIR/darch $ROOTFS/usr/share/initramfs-tools/scripts/darch
 chroot $ROOTFS update-initramfs -u
 
 # https://stackoverflow.com/questions/6710555/how-to-use-qemu-to-run-a-non-gui-os-on-the-terminal
